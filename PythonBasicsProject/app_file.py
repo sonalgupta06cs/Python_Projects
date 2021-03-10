996e38658997e775c768124ccc47914997b61ac7
sonalgupta06cs <sonalgupta06cs@gmail.com>
2021-03-10 22:15:24 +0530

    First Commit

---
+++ PythonBasicsProject/app_file.py
@@ -0,0 +1,13 @@
+print('Sonal Gupta')
+# variables
+age = 32
+print('age', age)
+is_married = True
+print('is_married', is_married)
+spouse_name = 'Sachin Gupta'
+print('spouse_name', spouse_name)
+# Receiving Input from user from Terminal
+profession = input("Enter your profession? ")
+print("Hello, Your Profession " + profession + " seems great !")
+# Type Conversion from 1 type to another
+
