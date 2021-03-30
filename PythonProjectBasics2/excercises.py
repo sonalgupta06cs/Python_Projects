833aa8650a04a9e86649994e59e4b6828750e3a1
sonalgupta06cs <sonalgupta06cs@gmail.com>
2021-03-30 21:14:30 +0530

    added guess n car game code

--- PythonProjectBasics2/excercises.py
+++ PythonProjectBasics2/excercises.py
@@ -1,3 +1,49 @@
 weight_lbs = input("Enter your weight in pounds :- ")
 weight_kgs = int(weight_lbs) * 0.45
 print('weight in kgs ' + str(weight_kgs))
+
+# House value to put down by x%
+price_house = 1000000
+has_good_credits = True
+if has_good_credits:
+    down_payment = 0.1 * price_house
+else:
+    down_payment = 0.2 * price_house
+print("The down payment needs to be made for : ", f'Down payment: $ {down_payment}')
+
+##############################################
+# Guessing Game, secret number set to 9
+secret_no = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_no:
+        print("You won!")
+        break
+else: # else block for while, in python we can have else for while loop after while loop runs
+      # successfully, it gets executed.
+    print("Sorry, You Failed!")
+
+
+##############################################
+# Car Game
+command = ""
+while True:
+    command = input("> ").lower()
+    if command == 'start':
+        print("Car Started, Ready to go!")
+    elif command == 'stop':
+        print("Car Stopped.")
+    elif command == 'help':
+        print('''start - to start the car
+stop - to stop the car
+quit - to quit the car''')
+    elif command == 'quit':
+        break
+    else:
+        print("Sorry, I don't understand that!")
+
+
+
