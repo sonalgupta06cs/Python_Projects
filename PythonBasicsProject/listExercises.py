f2e04a5dcd36cbbe20e41bc73bc7ccc9b09eaf77
sonalgupta06cs <sonalgupta06cs@gmail.com>
2021-06-06 01:09:45 +0530

    added lists methods and its excercise.

--- PythonBasicsProject/listExercises.py
+++ PythonBasicsProject/listExercises.py
@@ -8,3 +8,31 @@ for number in numbers:
         continue
 print("max number is ", maxNum)
 
+
+# Remove the duplicates from the list - 1
+numbersList1 = [2, 2, 4, 6, 3, 4, 6, 1]
+uniques = []
+counter = 0;
+for number in numbersList1:
+    if number not in uniques:
+        uniques.insert(counter, number)
+        counter += 1
+    else:
+        continue
+print("unique list of elements in the list 1 are ", uniques)
+
+
+# Remove the duplicates from the list - 2
+numbersList2 = [2, 2, 4, 6, 3, 4, 6, 1]
+uniques = []
+for number in numbersList2:
+    if number not in uniques:
+        uniques.append(number)
+    else:
+        continue
+print("unique list of elements in the list 2 are ", uniques)
+
+
+
+
+
--- PythonBasicsProject/lists.py
+++ PythonBasicsProject/lists.py
@@ -1,10 +1,3 @@
-# Tuples are like lists, but they are immutable
-#tuples_lists = (1, 2, 3)
-# [0] = 10 # error cant be changes as it is immutable
-#tuples_lists.count(1)
-#tuples_lists.index() # returns the index of the elements
-
-
 # Lists, a list of elementsround
 print("----------List Method Operations-----------------")
 names = ["John", "Bob", "Mosh", "Sam", "Marry"]
@@ -18,14 +11,25 @@ print("selected number of elements: " + str(names[0:3])) # last index is excludi
 
 ##############################################
 # List Methods, string in python are objects, list are also objects, so they also have objects
-numbers = [1, 2, 3, 4, 5, 6]
+numbers = [1, 2, 3, 4, 5, 6, 5]
 numbers.append(7)
 numbers.insert(3, 2) # index, element
 print("the list of numbers is : " + str(numbers))
 numbers.remove(2) # 2 from the list
 print("the updated list of numbers is : " + str(numbers))
-print(1 in numbers)
-print(len(numbers)) # size of the list
+print("pop", numbers.pop()) # removes the last element from the list
+# Check existence/occurrence of an item in the list using index/in operator
+print("Occurrence of a element 5 in list ", numbers.index(5))
+# print("Occurrence of a element not exist in list ", numbers.index(50)) # Error not in the list
+print("Occurrence of a element 5 in list ", 5 in numbers)
+print("Count the Occurrences of a element 5 in list ", numbers.count(5))
+print("Size of a list ", len(numbers)) # size of the list
+numbers.sort() # sorting of the list, be default in ascending order, call reverse method to sort in descending
+print("Sort the list in ascending by default ", numbers)
+numbers.reverse()
+print("Sort the list in descending ", numbers)
+anotherList = numbers.copy()
+print("Copy Method to copy the entire list ", anotherList)
 numbers.clear() # clears everything from the list
 print("final list of numbers: "+str(numbers))
 
